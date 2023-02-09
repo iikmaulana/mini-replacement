901811771dafd521bb892167fcbec63f0d4fe0b8
lana <iikmaulana369@gmail.com>
2023-02-10 00:41:10 +0700

    Add handler vehicle group

--- base/service/grpc/handler_vehilce.go
+++ base/service/grpc/handler_vehilce.go
@@ -63,3 +63,49 @@ func (handler VehicleHandler) UpdateMtVehicle(ctx context.Context, form *packets
 
 	return output, nil
 }
+
+func (handler VehicleHandler) CreateVehicleGroup(ctx context.Context, form *packets.VehicleReplacementRequest) (output *packets.VehicleReplacementOutput, err error) {
+	output = &packets.VehicleReplacementOutput{
+		Status: 0,
+	}
+
+	tmpResult, serr := handler.vehicleUsecase.CreateVehicleGroup(form.Data.Value)
+	if serr != nil {
+		return output, serr
+	}
+
+	b, err := json.Marshal(&tmpResult)
+	if err != nil {
+		return output, serror.NewFromError(err)
+	}
+
+	output.Status = 1
+	output.Data = &any.Any{
+		Value: b,
+	}
+
+	return output, nil
+}
+
+func (handler VehicleHandler) UpdateVehicleGroup(ctx context.Context, form *packets.VehicleReplacementRequest) (output *packets.VehicleReplacementOutput, err error) {
+	output = &packets.VehicleReplacementOutput{
+		Status: 0,
+	}
+
+	tmpResult, serr := handler.vehicleUsecase.UpdateVehicleGroup(form.Data.Value)
+	if serr != nil {
+		return output, serr
+	}
+
+	b, err := json.Marshal(&tmpResult)
+	if err != nil {
+		return output, serror.NewFromError(err)
+	}
+
+	output.Status = 1
+	output.Data = &any.Any{
+		Value: b,
+	}
+
+	return output, nil
+}
